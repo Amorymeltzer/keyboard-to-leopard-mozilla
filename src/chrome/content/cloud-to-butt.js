1b4207c1995f3c87efdc1d4aefbc2ca0d922418c
Ioan Loosley <legit.ioangogo@gmail.com>
2014-10-21 12:46:06 +0100

    Update cloud-to-butt.js

--- src/chrome/content/cloud-to-butt.js
+++ src/chrome/content/cloud-to-butt.js
@@ -31,10 +31,10 @@
     {
         var v = textNode.nodeValue;
     
-        v = v.replace(/\bThe Cloud\b/g, "My Butt");
-        v = v.replace(/\bThe cloud\b/g, "My butt");
-        v = v.replace(/\bthe Cloud\b/g, "my Butt");
-        v = v.replace(/\bthe cloud\b/g, "my butt");
+    	v = v.replace(/\bEbola\b/g, "Zombie Virus");
+	    v = v.replace(/\bebola\b/g, "zombie virus");
+	    v = v.replace(/\bEBOLA\b/g, "ZOMBIE VIRUS");
+	    v = v.replace(/\bCDC\b/g, "Incompitent agency");
     
         textNode.nodeValue = v;
     }
