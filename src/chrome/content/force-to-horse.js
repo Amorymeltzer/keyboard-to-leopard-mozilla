5453aa435d7156b6d40fbf0bc8402eeb0c78577f
Amory Meltzer <Amorymeltzer@gmail.com>
2014-10-23 14:42:38 -0700

    Different version

--- src/chrome/content/force-to-horse.js
+++ src/chrome/content/force-to-horse.js
@@ -2,43 +2,63 @@
 
     function walk(node)
     {
-        // I stole this function from here:
-        // http://is.gd/mwZp7E
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
 
-        var child, next;
+	var child, next;
 
-        switch ( node.nodeType )
-        {
-            case 1:  // Element
-            case 9:  // Document
-            case 11: // Document fragment
-            child = node.firstChild;
-            while ( child )
-            {
-                next = child.nextSibling;
-                walk(child);
-                child = next;
+	switch ( node.nodeType )
+	{
+	    case 1:  // Element
+	    case 9:  // Document
+	    case 11: // Document fragment
+	    child = node.firstChild;
+	    while ( child )
+	    {
+		next = child.nextSibling;
+		walk(child);
+		child = next;
+	    }
+	    break;
+
+	    case 3: // Text node
+            if(node.parentElement.tagName.toLowerCase() != "script") {
+                handleText(node);
             }
-            break;
-
-            case 3: // Text node
-            handleText(node);
-            break;
-        }
+	    break;
+	}
     }
 
-    function handleText(textNode)
-    {
-        var v = textNode.nodeValue;
+    function handleText(textNode) {
+	var v = textNode.nodeValue;
 
-	v = v.replace(/\bForce\b/g, "Horse");
-	v = v.replace(/\force\b/g, "horse");
-	v = v.replace(/\bFORCE\b/g, "HORSE");
-	v = v.replace(/\bBattalion\b/g, "Stallion");
-	v = v.replace(/\bbattalion\b/g, "stallion");
-	v = v.replace(/\bBATTALION\b/g, "STALLION");
+	// Deal with the easy case
+	v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
+	    // t - 7 = m
+	    // c - 1 = b
+	    m = String.fromCharCode(p1.charCodeAt(0) - 7);
+	    b = String.fromCharCode(p2.charCodeAt(0) - 1);
+	    return m + "y " + b + "utt";
+	});
 
-        textNode.nodeValue = v;
+	// Deal with private clouds
+	v = v.replace(/\b(M|m)exic(o)/g, function(match, p1, p2, offset, string) {
+	    // c - 1 = b
+	    b = String.fromCharCode(p2.charCodeAt(0) - 1);
+	    return b + "utt";
+	});
+	// Get the corner cases
+	if(v.match(/cloud/i)) {
+	    // If we're not talking about weather
+	    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
+		v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
+		    // c - 1 = b
+		    b = String.fromCharCode(p1.charCodeAt(0) - 1);
+		    return b + "utt";
+		});
+	    }
+	}
+	textNode.nodeValue = v;
     }
 
     function windowLoadHandler()
