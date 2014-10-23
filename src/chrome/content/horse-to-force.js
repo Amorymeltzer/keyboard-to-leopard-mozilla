2611fbfd7460a0f4366ea5556643aafa30db8c83
Amory Meltzer <Amorymeltzer@gmail.com>
2014-10-23 12:27:02 -0700

    Initial update, but I did this backasswards

--- src/chrome/content/horse-to-force.js
+++ src/chrome/content/horse-to-force.js
@@ -1,41 +1,41 @@
 (function() {
 
-    function walk(node) 
+    function walk(node)
     {
         // I stole this function from here:
         // http://is.gd/mwZp7E
-    
+
         var child, next;
-    
-        switch ( node.nodeType )  
+
+        switch ( node.nodeType )
         {
             case 1:  // Element
             case 9:  // Document
             case 11: // Document fragment
                 child = node.firstChild;
-                while ( child ) 
+                while ( child )
                 {
                     next = child.nextSibling;
                     walk(child);
                     child = next;
                 }
                 break;
-    
+
             case 3: // Text node
                 handleText(node);
                 break;
         }
     }
-    
+
     function handleText(textNode)
     {
         var v = textNode.nodeValue;
-    
-    	v = v.replace(/\bEbola\b/g, "Zombie Virus");
-	    v = v.replace(/\bebola\b/g, "zombie virus");
+
+	v = v.replace(/\bHorse\b/g, "Force");
+	    v = v.replace(/\bebola\b/g, "force");
 	    v = v.replace(/\bEBOLA\b/g, "ZOMBIE VIRUS");
 	    v = v.replace(/\bCDC\b/g, "Incompitent agency");
-    
+
         textNode.nodeValue = v;
     }
 
